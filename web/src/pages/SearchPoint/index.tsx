269fd5d07b5b03f68d5465b4a42d59311913fbbe
Note45 <antonion.2048@gmail.com>
2020-06-08 10:00:55 -0300

    Created function handleClick to page SearchPoint in web application

--- web/src/pages/SearchPoint/index.tsx
+++ web/src/pages/SearchPoint/index.tsx
@@ -1,13 +1,50 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent, useEffect} from 'react';
+import axios from 'axios';
 
 import './styles.css';
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+
+interface IBGECityResponse {
+  nome: string;
+}
 
 const SearchPoint = () => {
   const [ufs, setUfs] = useState<string[]>([]);
   const [cities, setCities] = useState<string[]>([]);
 
-  const [selectedUf, setSelectedUf] = useState('0');
-  const [selectedCity, setSelectedCity] = useState('0');
+  const [selectedUf, setSelectedUf] = useState('');
+  const [selectedCity, setSelectedCity] = useState('');
+
+  useEffect(() => {
+    axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+      .then(response => {
+        const ufInitials = response.data.map(uf => uf.sigla);
+
+        setUfs(ufInitials);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+    .then(response => {
+      const cityNames = response.data.map(city => city.nome);
+
+      setCities(cityNames);
+    });
+  }, [selectedUf]);
+
+  async function handleClick() {
+    console.log('Entrou HandleCLick')
+    if(ufs.includes(selectedUf.toUpperCase()) === false || cities.includes(selectedCity) === false) {
+      alert('Erro no valor do estado ou cidade!');
+      return;
+    }
+
+    //go to page ListPointsSearch
+  }
 
   function handleSelectedUf(event: ChangeEvent<HTMLInputElement>) {
     const uf = event.target.value;
@@ -25,21 +62,27 @@ const SearchPoint = () => {
     <div className='containerSearchPoint'>
       <input
         type='text'
-        name='city'
-        id='city'
-        onChange={handleSelectedCity}
-        placeholder='Digite a Cidade'
-      />
-
-      <input
-        type='text'
         name='Estado'
         id='uf'
         onChange={handleSelectedUf}
         placeholder='Digite o estado(UF)'
+        maxLength={2}
+        required
+      />
+
+      <input
+        type='text'
+        name='city'
+        id='city'
+        onChange={handleSelectedCity}
+        placeholder='Digite a Cidade'
+        required
       />
       
-      <button className='buttonSearch'>
+      <button 
+        className='buttonSearch'
+        onClick={handleClick}
+      >
         <p className='buttonText'>Buscar</p>
       </button>
     </div>
