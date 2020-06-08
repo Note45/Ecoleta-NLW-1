5de7bb04e5fd0c35295f58f1c5ec6dc19b2b3089
Note45 <antonion.2048@gmail.com>
2020-06-08 08:55:16 -0300

    Created styles to page SearchPoint

--- web/src/pages/SearchPoint/index.tsx
+++ web/src/pages/SearchPoint/index.tsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
+
+import './styles.css';
 
 const SearchPoint = () => {
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [cities, setCities] = useState<string[]>([]);
+
+  const [selectedUf, setSelectedUf] = useState('0');
+  const [selectedCity, setSelectedCity] = useState('0');
+
+  function handleSelectedUf(event: ChangeEvent<HTMLInputElement>) {
+    const uf = event.target.value;
+
+    setSelectedUf(uf);
+  }
+
+  function handleSelectedCity(event: ChangeEvent<HTMLInputElement>) {
+    const city = event.target.value;
+
+    setSelectedCity(city);
+  }
+  
   return(
-    <h1>Página SearchPoint</h1>
+    <div className='containerSearchPoint'>
+      <input
+        type='text'
+        name='city'
+        id='city'
+        onChange={handleSelectedCity}
+        placeholder='Digite a Cidade'
+      />
+
+      <input
+        type='text'
+        name='Estado'
+        id='uf'
+        onChange={handleSelectedUf}
+        placeholder='Digite o estado(UF)'
+      />
+      
+      <button className='buttonSearch'>
+        <p className='buttonText'>Buscar</p>
+      </button>
+    </div>
   );
 }
 
