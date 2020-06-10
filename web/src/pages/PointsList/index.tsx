2f131c4fc741826fec00ae42bb03e23b8383a0b2
Note45 <antonion.2048@gmail.com>
2020-06-10 09:12:03 -0300

    Created useEffect at page PointList in web application to get filtered points in the API

--- web/src/pages/PointsList/index.tsx
+++ web/src/pages/PointsList/index.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import api from '../../services/api';
+
+interface Point {
+  city: string;
+  email: string;
+  id: number;
+  image: string;
+  image_url: string;
+  latitude: number;
+  longitude: number;
+  name: string;
+  uf: string;
+  whatsapp: string;
+}
 
 const PointsList = () => {
+  const [ pointsId, setPointsId ] = useState<number[]>([]);
+
   const { Data }: any = useLocation().state;
+
+  useEffect(() => {
+    api.get(`/points?city=${Data.selectedCity}&uf=${Data.selectedUf}&items=1,2,3,4,5,6`)
+      .then(response => {
+        let ids = response.data.map((point: Point) => point.id);
+
+        setPointsId(ids);
+      })
+  }, []);
 
   return (
     <h1>Page Points List</h1>
